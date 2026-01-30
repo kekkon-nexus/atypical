5839630e1310b4737c5ee97b3160ed342ead4bbb
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-01-30 22:17:13 +0700

    add(lib)[int]: parser structures

---
+++ crates/atypical-commit/src/lib.rs
@@ -0,0 +1,77 @@
+use chumsky::extra::State;
+
+/// The modifier of a prefix.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Modifier {
+    /// The symbol `?`.
+    Question,
+    /// The symbol `!`, with the number of exclamation marks.
+    /// Also known as the "breaking change" in Conventional Commits.
+    Exclamation(usize),
+}
+
+/// The kind of the enclosure delimiters.
+///
+/// See [Enclosure].
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Delimiter {
+    /// The delimiters `(` and `)`.
+    /// Is used for the "scope" in Conventional Commits.
+    Round,
+    /// The delimiters `[` and `]`.
+    Square,
+}
+
+/// An enclosure of content within delimiters.
+#[derive(Debug, Clone, PartialEq)]
+pub struct Enclosure<'i> {
+    pub delimiter: Delimiter,
+    pub content: &'i str,
+}
+
+/// The prefix of a commit message header.
+#[derive(Debug, Clone, PartialEq)]
+pub struct Prefix<'i> {
+    /// Also known as the "type" in Conventional Commits.
+    pub keyword: &'i str,
+    /// Also known is the "breaking change" in Conventional Commits.
+    pub modifier: Option<Modifier>,
+    /// Also known as the "scope" in Conventional Commits.
+    pub enclosures: Vec<Enclosure<'i>>,
+}
+
+/// The header of a commit message. Contains a pair of (prefix, summary).
+pub type Header<'i> = (Prefix<'i>, &'i str);
+
+/// The body of a commit message. Contains the raw text.
+pub type Body<'i> = &'i str;
+
+/// The trailers of a commit message. Contains a sequence of (key, text) pairs.
+pub type Trailers<'i> = Vec<(&'i str, &'i str)>;
+
+/// The location of the modifier symbol.
+///
+/// See [Modifier].
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+pub enum ModifierLocation {
+    /// Appears directly after the keyword and before enclosures.
+    Before,
+    /// Appears after enclosures.
+    /// This is the Conventional Commits style.
+    After,
+    /// Appears either before or after enclosures.
+    /// It may only appear once.
+    #[default]
+    Either,
+}
+
+/// The state of the parser.
+#[derive(Debug, Clone, PartialEq, Default)]
+pub struct ExtraState {
+    pub modifier_location: ModifierLocation,
+}
+
+/// The parser extras.
+///
+/// See [chumsky::extra::ParserExtra].
+pub type Extra = State<ExtraState>;
