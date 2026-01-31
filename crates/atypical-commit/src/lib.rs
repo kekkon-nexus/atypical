6f46736e14f0c11cf74e27edccdb538be6cb43b2
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-01-31 21:14:44 +0700

    add(doc): improve documentations

--- crates/atypical-commit/src/lib.rs
+++ crates/atypical-commit/src/lib.rs
@@ -4,9 +4,10 @@ use chumsky::prelude::*;
 /// The position of the modifier.
 ///
 /// It is an enum flag and can be used as a bitmask.
-/// [Self::default] allows all positions.
+/// [`Self::default`] allows all positions.
 ///
-/// See [struct@Modifier].
+/// See [`struct@Modifier`].
+#[doc(alias("modifier", "importance", "position", "location"))]
 #[bitflag(u8)]
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Hash)]
 pub enum ModifierPosition {
@@ -26,7 +27,8 @@ impl Default for ModifierPosition {
 
 /// The kind of the modifier.
 ///
-/// See [struct@Modifier].
+/// See [`struct@Modifier`].
+#[doc(alias("modifier", "importance", "kind", "type"))]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum ModifierKind {
     /// The symbol `?`.
@@ -37,6 +39,7 @@ pub enum ModifierKind {
 }
 
 /// The modifier of a prefix.
+#[doc(alias("modifier", "importance"))]
 #[derive(Debug, Clone, PartialEq)]
 pub struct Modifier {
     // Should be guaranteed to be XOR-compatible.
@@ -47,16 +50,19 @@ pub struct Modifier {
 /// The kind of an enclosure delimiters.
 ///
 /// It is an enum flag and can be used as a bitmask.
-/// [Self::default] allows all delimiters.
+/// [`Self::default`] allows all delimiters.
 ///
-/// See [struct@Enclosure].
+/// See [`struct@Enclosure`].
+#[doc(alias("scope"))]
 #[bitflag(u8)]
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Hash)]
 pub enum Delimiter {
     /// The delimiters `(` and `)`.
     /// Is used for the "scope" in Conventional Commits.
+    #[doc(alias("parenthesis"))]
     Round = 1 << 0,
     /// The delimiters `[` and `]`.
+    #[doc(alias("bracket"))]
     Square = 1 << 1,
 }
 
@@ -68,6 +74,7 @@ impl Default for Delimiter {
 }
 
 /// An enclosure of content within delimiters.
+#[doc(alias("scope"))]
 #[derive(Debug, Clone, PartialEq)]
 pub struct Enclosure<'i> {
     // Should be guaranteed to be XOR-compatible.
@@ -76,6 +83,7 @@ pub struct Enclosure<'i> {
 }
 
 /// The prefix of a commit message header.
+#[doc(alias("type", "verb"))]
 #[derive(Debug, Clone, PartialEq)]
 pub struct Prefix<'i> {
     /// Also known as the "type" in Conventional Commits.
@@ -93,10 +101,12 @@ pub type Header<'i> = (Prefix<'i>, &'i str);
 pub type Body<'i> = &'i str;
 
 /// The trailers of a commit message. Contains a sequence of (key, text) pairs.
+#[doc(alias("footer"))]
 pub type Trailers<'i> = Vec<(&'i str, &'i str)>;
 
 /// The general context of the parser.
 // The default should allow all flags.
+#[doc(alias("config", "settings"))]
 #[derive(Debug, Clone, PartialEq, Default)]
 pub struct ExtraContext {
     pub allowed_modifier_position: ModifierPosition,
@@ -106,9 +116,24 @@ pub struct ExtraContext {
 /// The parser extras.
 ///
 /// See [extra::ParserExtra].
+#[doc(alias("config", "settings"))]
 pub type Extra = extra::Context<ExtraContext>;
 
-/// Parses a modifier.
+/// Parses a [`ModifierKind`].
+///
+/// # Examples
+///
+/// ```
+/// assert_eq!(
+///     modifier().parse("?").into_result(),
+///     Ok(ModifierKind::Question)
+/// );
+///
+/// assert_eq!(
+///     modifier().parse("!!").into_result(),
+///     Ok(ModifierKind::Exclamation(2))
+/// );
+/// ```
 pub fn modifier<'i>() -> impl Parser<'i, &'i str, ModifierKind> {
     choice((
         just('?').to(ModifierKind::Question),
@@ -120,8 +145,28 @@ pub fn modifier<'i>() -> impl Parser<'i, &'i str, ModifierKind> {
     ))
 }
 
-/// Parses an enclosure.
-/// For contextual parsing, use [fn@enclosure_with_ctx].
+/// Parses an [`Enclosure`].
+///
+/// For contextual parsing, use [`fn@enclosure_with_ctx`].
+///
+/// # Examples
+///
+/// ```
+/// assert_eq!(
+///     enclosure().parse("(example)").into_result(),
+///     Ok(Enclosure {
+///         delimiter: Delimiter::Round,
+///         content: "example",
+///    })
+/// );
+///
+/// assert_eq!(
+///     enclosure().parse("[(\t]").into_result(),
+///     Ok(Enclosure {
+///         delimiter: Delimiter::Square,
+///         content: "(\t",
+///     })
+/// );
 pub fn enclosure<'i>() -> impl Parser<'i, &'i str, Enclosure<'i>, extra::Context<Delimiter>> {
     fn dry<'i>(
         start: char,
@@ -143,9 +188,30 @@ pub fn enclosure<'i>() -> impl Parser<'i, &'i str, Enclosure<'i>, extra::Context
     ))
 }
 
-/// Parses an enclosure with a given delimiter context.
-/// Accepts a parameter of allowed delimiters.
-/// For generic parsing, use [fn@enclosure].
+/// Parses an [`Enclosure`] with a given delimiter context.
+/// Accepts a parameter of allowed [delimiters](Delimiter).
+///
+/// For generic parsing, use [`fn@enclosure`].
+///
+/// # Examples
+///
+/// ```
+/// assert_eq!(
+///     enclosure_with_ctx(Delimiter::Round)
+///         .parse("(example)")
+///         .into_result(),
+///     Ok(Enclosure {
+///         delimiter: Delimiter::Round,
+///         content: "example",
+///     })
+/// );
+///
+/// assert!(
+///     enclosure_with_ctx(Delimiter::Square)
+///         .parse("(fail)")
+///         .has_errors()
+/// );
+/// ```
 pub fn enclosure_with_ctx<'i>(
     delimiter: Delimiter,
 ) -> impl Parser<'i, &'i str, Enclosure<'i>, extra::Context<Delimiter>> {
