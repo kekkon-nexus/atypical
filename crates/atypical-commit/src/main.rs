6655ec21d9e76f17c9c1fc20f10d294a2af3179f
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-05-18 17:33:36 +0700

    add(bin): initial error reporting implementation using `ariadne`

--- crates/atypical-commit/src/main.rs
+++ crates/atypical-commit/src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use ariadne::{ColorGenerator, Label, Report, ReportKind, Source};
+use clap::Parser;
+use clap_stdin::FileOrStdin;
+
+#[derive(Debug)]
+#[derive(Parser)]
+#[command(
+    version,
+    about = "Lint commit messages with atypical.",
+    long_about = "\
+        Lint commit messages with atypical.\n\
+        Check out the documentation for more details: https://github.com/kekkon-nexus/atypical\
+    "
+)]
+struct Args {
+    input: Option<FileOrStdin>,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    if let Some(input) = args.input {
+        let filename = input.filename().to_owned();
+        let input = input.contents()?;
+
+        println!("Input: {}", input);
+
+        use chumsky::Parser;
+        let result = atypical_commit::prefix().parse(&input);
+
+        if result.has_errors() {
+            let mut report =
+                Report::build(ReportKind::Error, (&filename, 0..input.len()))
+                    .with_message("Failed to parse commit message")
+                    .with_code(3);
+
+            let mut colors = ColorGenerator::new();
+
+            for error in result.errors() {
+                report = report
+                .with_label(
+                    Label::new((&filename, error.span().into_range()))
+                        .with_message("Invalid commit message")
+                        .with_color(colors.next()),
+                );
+            }
+
+            report.finish().print((&filename, Source::from(&input)))?;
+        }
+    } else {
+        println!("No input provided.");
+    }
+
+    Ok(())
 }
